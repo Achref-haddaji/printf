631f934c6a790acd7e04cef1bed88a1b42d55591
Achref-haddaji <haddaji.achref19@gmail.com>
2022-03-15 21:38:21 +0100

    last updated

--- printf.c
+++ printf.c
@@ -9,7 +9,6 @@
  */
 int _printf(const char *fmt, ...)
 {
-
 unsigned int i;
 int count = 0;
 char l;
@@ -64,61 +63,6 @@ va_list argp;
 			fmt++;
 		}
 	}
-	va_end (argp);
+	va_end(argp);
 	return (count);
-    unsigned int i;
-
-    int count = 0 ;
-    char l;
-    int o;
-    char *ch;
-    va_list argp;
-
-    if (fmt == NULL || fmt[0] == '\0')
-    {
-        return(-1);
-    }
-    va_start(argp, fmt);
-    while (*fmt)
-    {
-        if (*fmt != '%')
-        {   
-        count++;
-        _putchar(*fmt);
-        fmt++;
-        }
-        else
-        {
-            fmt++;
-            if (*fmt == 'c')
-            {
-                l = va_arg(argp, int);
-                count ++;
-                _putchar(l);
-            }
-            else if (*fmt == 's')
-            {
-                ch = va_arg(argp, char*);
-                count += printstring(ch);
-            }
-            else if (*fmt == 'd')
-            {
-             o = va_arg(argp, int);
-             count+= _printnumber(o);
-
-            }
-            else if (*fmt == 'i')
-            {
-             o = va_arg(argp, int);
-             count+= _printnumber(o);
-            }
-            else if (*fmt == '%')
-            {
-                _putchar('%');
-            }
-            fmt++;
-        }
-    }
-    va_end (argp);
-    return (count);
 }
