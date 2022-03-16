68fb65dedff28e801086e8ce8c3062d9c647867e
abdou50 <abdelkaderraddadi988@gmail.com>
2022-03-16 19:50:36 +0200

    l

--- printnumber.c
+++ printnumber.c
@@ -1,4 +1,11 @@
 #include "main.h"
+/**
+ * _printnumber - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int _printnumber(int n)
 {
     int count = 0, rest = 1;
